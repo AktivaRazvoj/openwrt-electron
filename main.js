42c06a29d2a8d9830f55309bb957ba4066ef46eb
Aktiva Razvoj - Timotej Medved <razvoj@aktiva.si>
2025-04-30 17:14:12 +0200

    installer

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const { handleSSHCommands } = require('./src/api');
 
-const { autoUpdater } = require('electron-updater');
+
 const { dialog } = require('electron');
 
 
@@ -12,6 +12,7 @@ function createWindow() {
     height: 700,
     resizable: false,
     autoHideMenuBar: true,
+    icon: path.join(__dirname, 'assets', 'icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
@@ -25,32 +26,7 @@ function createWindow() {
 app.whenReady().then(() => {
   createWindow();
 
-  // Check for updates after app is ready
-  autoUpdater.checkForUpdatesAndNotify();
 
-  // Optional: Listen for update events
-  autoUpdater.on('update-available', () => {
-    dialog.showMessageBox({
-      type: 'info',
-      title: 'Na voljo je posodobitev',
-      message: 'Na voljo je nova različica. Prenašam ...',
-    });
-  });
-
-  autoUpdater.on('update-downloaded', () => {
-    dialog.showMessageBox({
-      type: 'info',
-      title: 'Posodobitev pripravljena',
-      message: 'Nova različica je bila prenesena. Želite ponovno zagnati za namestitev?',
-      buttons: ['Ponovni zagon', 'Kasneje'],
-    }).then(result => {
-      if (result.response === 0) autoUpdater.quitAndInstall();
-    });
-  });
-
-  autoUpdater.on('error', (err) => {
-    dialog.showErrorBox('Napaka pri posodobitvi', err == null ? "neznano" : (err.stack || err).toString());
-  });
 });
 
 
